feb0b7ba56f2fab8b0b4f4f0f70f9949181f8eb0
John Fa <john6fa@gmail.com>
2021-12-21 15:03:05 -0800

    view controller work

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+=begin
+Application classes and modules are available everywhere, you do not need and should not load anything under app with require. This feature is called autoloading, and you can learn more about it in Autoloading and Reloading Constants.
+
+  You only need require calls for two use cases:
+  
+  To load files under the lib directory.
+  To load gem dependencies that have require: false in the Gemfile.
+
+=end
+
+class ArticlesController < ApplicationController
+  def index
+    @temp = "abc"
+  end
+end
