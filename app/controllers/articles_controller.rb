e686901d1e9d8c6267fd3297cac388ee3c7d28b8
John Fa <john6fa@gmail.com>
2021-12-22 11:52:42 -0800

    new form html erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,31 @@ Application classes and modules are available everywhere, you do not need and sh
   To load files under the lib directory.
   To load gem dependencies that have require: false in the Gemfile.
 
+  class ArticlesController < ApplicationController
+    def index
+      @articles = Article.all
+    end
+  
+    def show
+      @article = Article.find(params[:id])
+    end
+  
+    def new
+      @article = Article.new
+    end
+  
+    def create
+      @article = Article.new(title: "...", body: "...")
+  
+      if @article.save
+        redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
 =end
+
 
 class ArticlesController < ApplicationController
   def index
@@ -16,5 +40,28 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # puts params[:article] === {"title"=>"555", "body"=>"555"}
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      # require and permit. 
+      # The former is used to mark parameters as required. 
+      # The latter is used to set the parameter as permitted and limit which attributes should be allowed for mass updating.
+      params.require(:article).permit(:title, :body)
+    end
 end
+
