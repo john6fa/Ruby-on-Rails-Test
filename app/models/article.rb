865664a6ed336a63ff488992388a98977b4a961c
John Fa <john6fa@gmail.com>
2021-12-23 19:27:09 -0800

    authorization

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,14 @@
 class Article < ApplicationRecord
-    include Visible
-    has_many :comments
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
+  include Visible
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
+
   
 
 # One article can have many comments.
+# If you set the :dependent option to: :destroy, when the object is destroyed, destroy will be called on its associated objects
+# Articles have associated comment objects
